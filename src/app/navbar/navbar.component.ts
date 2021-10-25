1827126d61aba42abe8681e8aaaeb1cf84eb19b4
paulinemarg <paulinemargossian@gmail.com>
2021-10-25 15:10:17 +0200

    Create navbar

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+
+  constructor(public router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  toProfile(): void {
+    this.router.navigate(['/profile'])
+      .then(success => console.log('navigation success?', success))
+      .catch(console.error);
+  }
+
+  toMovies(): void {
+    this.router.navigate(['/movies'])
+      .then(success => console.log('navigation success?', success))
+      .catch(console.error);
+  }
+
+  toDirectors(): void {
+    this.router.navigate(['/directors'])
+      .then(success => console.log('navigation success?', success))
+      .catch(console.error);
+  }
+
+  toGenres(): void {
+    this.router.navigate(['/genres'])
+      .then(success => console.log('navigation success?', success))
+      .catch(console.error);
+  }
+
+  backToHome(): void {
+    this.router.navigate(['/movies'])
+      .then(success => console.log('navigation success?', success))
+      .catch(console.error);
+  }
+
+  logOut(): void {
+    localStorage.removeItem('user');
+    localStorage.removeItem('token');
+    this.router.navigate(['/welcome'])
+      .then(success => console.log('navigation success?', success))
+      .catch(console.error);
+  }
+}
