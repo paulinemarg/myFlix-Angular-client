d4312266c908c688be441b867b82f7d86d9daf49
paulinemarg <paulinemargossian@gmail.com>
2021-10-26 20:55:43 +0200

    created directors component

---
+++ src/app/directors/directors.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { FetchApiDataService } from '../fetch-api-data.service';
+
+@Component({
+  selector: 'app-directors',
+  templateUrl: './directors.component.html',
+  styleUrls: ['./directors.component.scss']
+})
+export class DirectorsComponent implements OnInit {
+  directors: any[] = [];
+
+  constructor(public fetchApiData: FetchApiDataService) { }
+
+  ngOnInit(): void {
+    this.getDirectors();
+  }
+
+  getDirectors(): void {
+    this.fetchApiData.getDirectors().subscribe((response: any) => {
+      this.directors = response;
+      // console.log(this.directors);
+      return this.directors;
+    });
+  }
+}
