5cf7e0627032b33d42c912d26fd8f2a97128eaac
paulinemarg <paulinemargossian@gmail.com>
2021-10-26 20:55:26 +0200

    created genres component

---
+++ src/app/genres/genres.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { FetchApiDataService } from '../fetch-api-data.service';
+
+@Component({
+  selector: 'app-genres',
+  templateUrl: './genres.component.html',
+  styleUrls: ['./genres.component.scss']
+})
+export class GenresComponent implements OnInit {
+  genres: any[] = [];
+
+  constructor(public fetchApiData: FetchApiDataService) { }
+
+  ngOnInit(): void {
+    this.getGenres();
+  }
+
+  getGenres(): void {
+    this.fetchApiData.getGenres().subscribe((response: any) => {
+      this.genres = response;
+      // console.log(this.genres);
+      return this.genres;
+    });
+  }
+}
