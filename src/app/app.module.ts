acc59b8040f9a70b6d0735f4c0856e99de4ebd89
paulinemarg <paulinemargossian@gmail.com>
2021-10-28 09:54:32 +0200

    changed the route path of directors & genres

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,41 +1,44 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes, } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// Adds the modules from the Material components
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatIconModule } from '@angular/material/icon';
 import { FormsModule } from '@angular/forms';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 import { MovieCardComponent } from './movie-card/movie-card.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
-import { RouterModule, Routes } from '@angular/router';
-import { MatIconModule } from '@angular/material/icon';
 import { UserProfileComponent } from './user-profile/user-profile.component';
-import { EditUserProfileComponent } from './edit-user-profile/edit-user-profile.component';
-import { MovieDirectorComponent } from './movie-director/movie-director.component';
-import { MovieGenreComponent } from './movie-genre/movie-genre.component';
 import { MovieSynopsisComponent } from './movie-synopsis/movie-synopsis.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { DirectorsComponent } from './directors/directors.component';
+import { MovieGenreComponent } from './movie-genre/movie-genre.component';
 import { GenresComponent } from './genres/genres.component';
+import { MovieDirectorComponent } from './movie-director/movie-director.component';
+import { DirectorsComponent } from './directors/directors.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { EditUserProfileComponent } from './edit-user-profile/edit-user-profile.component';
+import { AuthGuardService } from './auth-guard.service';
 
+// Welcome page is accessible when the user is not logged in.
 const appRoutes: Routes = [
-  { path: 'welcome', component: WelcomePageComponent },
+  { path: 'welcome', component: WelcomePageComponent, canActivate: [AuthGuardService] },
   { path: 'movies', component: MovieCardComponent },
-  { path: 'profile', component: UserProfileComponent },
-  { path: 'genres', component: GenresComponent },
   { path: 'directors', component: DirectorsComponent },
+  { path: 'genres', component: GenresComponent },
+  { path: 'profile', component: UserProfileComponent },
   { path: '', redirectTo: 'welcome', pathMatch: 'prefix' },
 ];
+
 
 @NgModule({
   declarations: [
@@ -45,13 +48,13 @@ const appRoutes: Routes = [
     MovieCardComponent,
     WelcomePageComponent,
     UserProfileComponent,
-    EditUserProfileComponent,
-    MovieDirectorComponent,
-    MovieGenreComponent,
     MovieSynopsisComponent,
-    NavbarComponent,
+    MovieGenreComponent,
+    GenresComponent,
+    MovieDirectorComponent,
     DirectorsComponent,
-    GenresComponent
+    NavbarComponent,
+    EditUserProfileComponent,
   ],
   imports: [
     RouterModule.forRoot(appRoutes),
@@ -60,15 +63,15 @@ const appRoutes: Routes = [
     AppRoutingModule,
     FormsModule,
     BrowserAnimationsModule,
-    MatButtonModule,
     MatDialogModule,
-    MatFormFieldModule,
-    MatIconModule,
     MatInputModule,
+    MatButtonModule,
     MatCardModule,
-    MatSnackBarModule
+    MatFormFieldModule,
+    MatSnackBarModule,
+    MatIconModule,
   ],
-  providers: [MovieCardComponent],
+  providers: [AuthGuardService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
